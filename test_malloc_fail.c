587e47a4a5d32fd8397e19975303157c79df8edb
Frantisek Sumsal <fsumsal@redhat.com>
2021-12-23 11:20:24 +0100

    Support calloc, realloc, and reallocarray as well

--- test_malloc_fail.c
+++ test_malloc_fail.c
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static uint64_t malloc_fail_at = 0;
+static int64_t malloc_fail_at = -1;
+static int64_t calloc_fail_at = -1;
+static int64_t realloc_fail_at = -1;
+static int64_t reallocarray_fail_at = -1;
 
 void *malloc(size_t size)
 {
     static void *(*real_malloc)(size_t);
-    static uint64_t malloc_counter = 0;
+    static int64_t malloc_counter = 0;
     char *env_malloc_fail_at = NULL;
 
     // TODO: replace atol() with something more secure
@@ -20,8 +23,68 @@ void *malloc(size_t size)
     if(!real_malloc)
         real_malloc = (typeof(real_malloc)) dlsym(RTLD_NEXT, "malloc");
 
-    if(malloc_counter++ >= malloc_fail_at)
+    if(malloc_fail_at >= 0 && malloc_counter++ >= malloc_fail_at)
         return real_malloc(-1);
 
     return real_malloc(size);
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+    static void *(*real_calloc)(size_t,size_t);
+    static int64_t calloc_counter = 0;
+    char *env_calloc_fail_at = NULL;
+
+    // TODO: replace atol() with something more secure
+    env_calloc_fail_at = secure_getenv("CALLOC_FAIL_AT");
+    if (env_calloc_fail_at)
+        calloc_fail_at = atol(env_calloc_fail_at);
+
+    if(!real_calloc)
+        real_calloc = (typeof(real_calloc)) dlsym(RTLD_NEXT, "calloc");
+
+    if(calloc_fail_at >= 0 && calloc_counter++ >= calloc_fail_at)
+        return real_calloc(nmemb, -1);
+
+    return real_calloc(nmemb, size);
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    static void *(*real_realloc)(void*,size_t);
+    static int64_t realloc_counter = 0;
+    char *env_realloc_fail_at = NULL;
+
+    // TODO: replace atol() with something more secure
+    env_realloc_fail_at = secure_getenv("REALLOC_FAIL_AT");
+    if (env_realloc_fail_at)
+        realloc_fail_at = atol(env_realloc_fail_at);
+
+    if(!real_realloc)
+        real_realloc = (typeof(real_realloc)) dlsym(RTLD_NEXT, "realloc");
+
+    if(realloc_fail_at >= 0 && realloc_counter++ >= realloc_fail_at)
+        return real_realloc(ptr, -1);
+
+    return real_realloc(ptr, size);
+}
+
+void *reallocarray(void *ptr, size_t nmemb, size_t size)
+{
+    static void *(*real_reallocarray)(void*,size_t,size_t);
+    static int64_t reallocarray_counter = 0;
+    char *env_reallocarray_fail_at = NULL;
+
+    // TODO: replace atol() with something more secure
+    env_reallocarray_fail_at = secure_getenv("REALLOCARRAY_FAIL_AT");
+    if (env_reallocarray_fail_at)
+        reallocarray_fail_at = atol(env_reallocarray_fail_at);
+
+    if(!real_reallocarray)
+        real_reallocarray = (typeof(real_reallocarray)) dlsym(RTLD_NEXT, "reallocarray");
+
+    if(reallocarray_fail_at >= 0 && reallocarray_counter++ >= reallocarray_fail_at)
+        return real_reallocarray(ptr, nmemb, -1);
+
+    return real_reallocarray(ptr, nmemb, size);
 }
