47db78bf9570fd2ef5400284cb9c3c108705cef6
Frantisek Sumsal <fsumsal@redhat.com>
2021-12-23 11:06:18 +0100

    Add rudimentary argument parsing

--- run_command.sh
+++ run_command.sh
@@ -7,9 +7,28 @@ ulimit -c 0
 
 MALLOC_MIN=0
 MALLOC_MAX=1000
-MALLOC_INC=1
+MALLOC_STEP=1
 
-for ((i = MALLOC_MIN; i < MALLOC_MAX; i += MALLOC_INC)); do
+while getopts "l:h:s:" opt; do
+    case "$opt" in
+        l)
+            MALLOC_MIN="$OPTARG"
+            ;;
+        h)
+            MALLOC_MAX="$OPTARG"
+            ;;
+        s)
+            MALLOC_STEP="$OPTARG"
+            ;;
+        *)
+            echo >&2 "Usage: $0 [-l MALLOC_MIN] [-h MALLOC_HIGH] [-s MALLOC_STEP]"
+            exit 1
+    esac
+done
+
+shift $((OPTIND - 1))
+
+for ((i = MALLOC_MIN; i < MALLOC_MAX; i += MALLOC_STEP)); do
    LD_PRELOAD=build/libtest_malloc_fail.so MALLOC_FAIL_AT=$i "$@" 
    if [[ $? -eq 139 ]]; then
        echo "Command '$*' failed with MALLOC_FAIL_AT=$i"
